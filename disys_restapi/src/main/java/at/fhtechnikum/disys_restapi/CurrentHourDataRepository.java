41c6306d1cd9d10f1e9c915c5c7a748dafe4e17f
WiIhelm <rogerwwilhelm@hotmail.de>
2025-06-25 18:45:20 +0200

    Refactoring for Restapi stuff.

--- disys_restapi/src/main/java/at/fhtechnikum/disys_restapi/CurrentHourDataRepository.java
+++ disys_restapi/src/main/java/at/fhtechnikum/disys_restapi/CurrentHourDataRepository.java
@@ -1,6 +1,15 @@
 package at.fhtechnikum.disys_restapi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
 
-public interface CurrentHourDataRepository extends JpaRepository<CurrentHourData, java.time.LocalDateTime> {
+/**
+ * Repository für den Zugriff auf aktuelle Verbrauchsdaten (CurrentHourData).
+ * Verwaltet Entities aus der Tabelle percentage_service.
+ *
+ * Primärschlüssel: LocalDateTime (hour)
+ */
+public interface CurrentHourDataRepository extends JpaRepository<CurrentHourData, LocalDateTime> {
+    // Eigene Query-Methoden können hier ergänzt werden, z.B.:
+    // Optional<CurrentHourData> findTopByOrderByHourDesc();
 }
